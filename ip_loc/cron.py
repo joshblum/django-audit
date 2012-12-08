7a362250bb44ae9eb08ba9628350db1851bdceca
Joshua Blum <joshblum@mit.edu>
2012-12-08 16:35:20 -0500

    added update user to cron, starting to write tests

--- ip_loc/cron.py
+++ ip_loc/cron.py
@@ -2,11 +2,15 @@ import kronos
 
 from ip_loc.cron_tasks.download_tor import DownloadTor
 from ip_loc.cron_tasks.download_ip import DownloadIP
+from ip_loc.cron_tasks.monitor_users import MonitorUsers
 
 @kronos.register("0 0 * * *")
 def daily_cron():
     ip = DownloadIP()
     ip.run()
+
+    user_monitor = MonitorUsers()
+    user_monitor.run()
 
 @kronos.register("0 * * * *")   
 def hourly_cron():
--- ip_loc/cron_tasks/test.py
+++
@@ -1,4 +0,0 @@
-from ip_loc.cron_tasks.monitor_users import MonitorUsers
-
-users = MonitorUsers()
-users.run()
