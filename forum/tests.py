1f54d4c9dddb0e205b045c985f1a66219d57f63c
Joshua Blum <joshblum@mit.edu>
2012-12-09 16:10:10 -0500

    test refactor

--- forum/tests.py
+++ forum/tests.py
@@ -6,35 +6,43 @@ from forum.models import *
 
 import time
 
-
-# class TestGet(unittest.TestCase):
-#     def setUp(self):
-#         user = User.objects.create_user('john', 'johnpassword')
-#         user.set_password("1234")
-#         user.save()
-#         self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-#         self.client.login(username='john', password='1234')
-
-#     def test_details(self):
-#         response = self.client.get('/admin/forum/forum/add/', {'title': 'Test', 'slug': 'Test', 'description': 'Test'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(len(response.context['customers']), 5)
+FORUM_ADD = '/admin/forum/forum/add/'
+THREAD_ADD = '/admin/forum/thread/add/'
+POST_ADD = '/admin/forum/post/add/'
 
 class TestBase(unittest.TestCase):
     def setUp(self):
-        user = User.objects.create_user(username='john', email='lennon@thebeatles.com', password='1234')
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
+        try:
+            self.user = User.objects.get(id=1) # user is already created.
+        except:
+            self.user = User.objects.create_user(username='john', email='lennon@thebeatles.com', password='1234')
+            self.user.is_superuser = True
+            self.user.is_staff = True
+            self.user.save()
 
-class TestForumCreation(TestBase):
-    def setUp(self):
-        super(TestForumCreation, self).setUp()
         self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
         self.client.login(username='john', password='1234')
 
+    def assertAuditCreation(self, obj_type, obj_count, audit_count):
+        """
+            Assert that the object type was created and that audit logs were created
+        """
+        self.assertEqual(obj_type.objects.all().count(), obj_count)
+        self.assertEqual(obj_type.audit_log.all().count(), audit_count)
+        self.assertAuditDetails(obj_type)
+
+    def assertAuditDetails(self, obj_type):
+        """
+            Assert that audit log details, user information and ip information are created
+        """
+        for i in obj_type.audit_log.all():
+            self.assertEqual(i.action_user.username, "john")
+            self.assertEqual(i.action_ip, "127.0.0.1")
+
+class TestForumCreation(TestBase):
+
     def test_details(self):
-        response = self.client.post('/admin/forum/forum/add/', 
+        response = self.client.post(FORUM_ADD,
             {
             'title': 'Test', 
             'slug': 'Test', 
@@ -43,21 +51,12 @@ class TestForumCreation(TestBase):
             'ordering': '',
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Forum.objects.all()), 1)
-        self.assertEqual(len(Forum.audit_log.all()), 1)
-        for i in Forum.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        self.assertAuditCreation(Forum, 1, 1)
 
-class TestForumThread(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumThread, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+class TestForumThread(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/thread/add/',
+        response = self.client.post(THREAD_ADD,
             {
             'forum': '1',
             'title': 'Test 1234',
@@ -67,21 +66,12 @@ class TestForumThread(unittest.TestCase):
             'latest_post_time_1': time.strftime("%H:%M:%S"),
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Thread.objects.all()), 1)
-        self.assertEqual(len(Thread.audit_log.all()), 1)
-        for i in Thread.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        self.assertAuditCreation(Thread, 1, 1)
 
-class TestForumThread2(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumThread2, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+class TestForumThread2(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/thread/add/',
+        response = self.client.post(,
             {
             'forum': '1',
             'title': 'Test 5678',
@@ -91,21 +81,14 @@ class TestForumThread2(unittest.TestCase):
             'latest_post_time_1': time.strftime("%H:%M:%S"),
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Thread.objects.all()), 2)
-        self.assertEqual(len(Thread.audit_log.all()), 2)
-        for i in Thread.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
 
-class TestForumCreation2(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumCreation2, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+        self.assertAuditCreation(Thread, 2, 2)
+
+
+class TestForumCreation2(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/forum/add/', 
+        response = self.client.post(FORUM_ADD, 
             {
             'title': 'Test2', 
             'slug': 'Test2', 
@@ -114,21 +97,13 @@ class TestForumCreation2(unittest.TestCase):
             'ordering': '',
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Forum.objects.all()), 2)
-        self.assertEqual(len(Forum.audit_log.all()), 2)
-        for i in Forum.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
 
-class TestForumThread3(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumThread3, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+        self.assertAuditCreation(Forum, 2, 2)
+
+class TestForumThread3(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/thread/add/',
+        response = self.client.post(THREAD_ADD,
             {
             'forum': '2',
             'title': 'Test 1234',
@@ -138,21 +113,12 @@ class TestForumThread3(unittest.TestCase):
             'latest_post_time_1': time.strftime("%H:%M:%S"),
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Thread.objects.all()), 3)
-        self.assertEqual(len(Thread.audit_log.all()), 3)
-        for i in Thread.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        self.assertAuditCreation(Thread, 3, 3)
 
-class TestForumThreadPost(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumPost, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+class TestForumThreadPost(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/post/add/',
+        response = self.client.post(POST_ADD,
             {
             'thread': '1',
             'author': '1',
@@ -161,21 +127,12 @@ class TestForumThreadPost(unittest.TestCase):
             'time_1': time.strftime("%H:%M:%S"),
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Post.objects.all()), 1)
-        self.assertEqual(len(Post.audit_log.all()), 2)
-        for i in Post.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        self.assertAuditCreation(Post, 1, 2)
 
-class TestForumThreadPost2(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumPost, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        print self.client.login(username='john', password='1234')
+class TestForumThreadPost2(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/post/add/',
+        response = self.client.post(POST_ADD,
             {
             'thread': '2',
             'author': '1',
@@ -184,21 +141,12 @@ class TestForumThreadPost2(unittest.TestCase):
             'time_1': time.strftime("%H:%M:%S"),
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Post.objects.all()), 2)
-        self.assertEqual(len(Post.audit_log.all()), 4)
-        for i in Post.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        self.assertAuditCreation(Post, 2, 4)
 
-class TestForumThreadPost3(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumPost, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+class TestForumThreadPost3(TestBase):
 
     def test_details(self):
-        response = self.client.post('/admin/forum/post/add/',
+        response = self.client.post(POST_ADD,
             {
             'thread': '1',
             'author': '1',
@@ -207,23 +155,10 @@ class TestForumThreadPost3(unittest.TestCase):
             'time_1': time.strftime("%H:%M:%S"),
             '_save': 'Save'
             })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(len(Post.objects.all()), 3)
-        self.assertEqual(len(Post.audit_log.all()), 6)
-        for i in Post.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        self.assertAuditCreation(Post, 3, 6)
 
-class TestForumThreadPostGet(unittest.TestCase):
-    def setUp(self):
-        #super(TestForumPost, self).setUp()
-        self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
-        self.client.login(username='john', password='1234')
+class TestForumThreadPostGet(TestBase):
 
     def test_details(self):
-        response = self.client.get('/admin/forum/post/1/')
-        self.assertEqual(len(Post.objects.all()), 3)
-        self.assertEqual(len(Post.audit_log.all()), 7)
-        for i in Post.audit_log.all():
-            self.assertEqual(i.action_user.username, "john")
-            self.assertEqual(i.action_ip, "127.0.0.1")
+        response = self.client.get('/admin/forum/post/1/') #test that read creates a log entry
+        self.assertAuditCreation(Post, 3, 7)
